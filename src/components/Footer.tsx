b1b58bbbe02467fa787b4b63d4fb756771c9f2a9
Peoplefirst <moocleadteam@gmail.com>
2025-07-13 00:31:18 +0200

    Fix missing React hooks imports

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Heart, Shield, Cookie, FileText, Mail, Phone, MapPin, Linkedin, Twitter, Facebook, UserCheck } from 'lucide-react';
 
